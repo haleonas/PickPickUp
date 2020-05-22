0f015a3d6b660907aaf9f5186665ae74c2943728
haleonas <altes@live.se>
2020-05-22 16:04:45 +0200

    added home after it disappeared

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <app-header title-text="Home"></app-header>
+</template>
+
+<script>
+    import Header from "../components/Header";
+    export default {
+        name: "Home",
+        components:{
+            appHeader: Header
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
