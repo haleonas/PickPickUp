b3a81eee30508d51d66d451b3aa0c9c0f2ab0f4c
haleonas <altes@live.se>
2020-05-22 09:43:25 +0200

    reworked filestructure
    reworked the filestructure to make it clearer what is what.
    
    added cors to the backend

---
+++ backend/express_server/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const sqlite = require('sqlite')
+const sqlite3 = require('sqlite3')
+const cors = require('cors')
+
+const app = express()
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('Hello from Pick & Pick up server')
+})
+
+app.get('/stores', (request, response) => {
+  sqlite
+    .open({ driver: sqlite3.Database, filename: './db/pickpickup.sqlite' })
+    .then(database => {
+      database.all('SELECT *FROM stores')
+        .then((rows) => {
+          response.status(201).send(rows)
+        })
+    })
+})
+
+app.listen(3000, () => {
+  console.log('Server is running')
+})
+
+
+
